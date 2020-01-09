9eaa2e3e77d2ff543aee2c666390a2ee8e255a81
liquidrinu <liquidrinu@gmail.com>
2020-01-09 19:04:11 +0100

    minor path fix

--- bin/ikea.sh
+++ bin/ikea.sh
@@ -3,11 +3,11 @@
 ### concat script-files to single file
 
 # initialize new file (overrides old bundle)
-rm '../medikb.html'
-touch '../medikb.html'
+rm './medikb.html'
+touch './medikb.html'
 
-cat "../assets/h1.html" >> ./medikb.html;
-cat "../src/styles.css" >> ./medikb.html;
-cat "../assets/h2.html" >> ./medikb.html;
-cat "../src/main.js" >> ./medikb.html;
-cat "../assets/h3.html" >> ./medikb.html;
+cat "./assets/h1.html" >> ./medikb.html;
+cat "./src/styles.css" >> ./medikb.html;
+cat "./assets/h2.html" >> ./medikb.html;
+cat "./src/main.js" >> ./medikb.html;
+cat "./assets/h3.html" >> ./medikb.html;
