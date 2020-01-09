23c609b675a3c24e03f1e2ac4694c2d70d14b858
liquidrinu <liquidrinu@gmail.com>
2020-01-09 21:25:18 +0100

    change file extensions in assets & added caps-lock

--- bin/ikea.sh
+++ bin/ikea.sh
@@ -6,8 +6,8 @@
 rm './medikb.html'
 touch './medikb.html'
 
-cat "./assets/h1.html" >> ./medikb.html;
+cat "./assets/h1.txt" >> ./medikb.html;
 cat "./src/styles.css" >> ./medikb.html;
-cat "./assets/h2.html" >> ./medikb.html;
+cat "./assets/h2.txt" >> ./medikb.html;
 cat "./src/main.js" >> ./medikb.html;
-cat "./assets/h3.html" >> ./medikb.html;
+cat "./assets/h3.txt" >> ./medikb.html;
