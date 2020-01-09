0ac11c6f90d588b3a1da968869e8b19d2058a3f8
liquidrinu <liquidrinu@gmail.com>
2020-01-09 22:25:37 +0100

    codepoint test

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ const customKeys = {
 
 // keybaord layout to generate
 const keyboard = [
-    ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", customKeys.backspace],
+    ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "\uD83D\uDE00"],
     ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", customKeys.clear],
     ["a", "s", "d", "f", "g", "h", "j", "k", "l", "?", customKeys.enter],
     ["z", "x", "c", "v", "b", "n", "m", customKeys.space, customKeys.shift, customKeys.yes, customKeys.no]
